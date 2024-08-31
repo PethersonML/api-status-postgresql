a1a9d2929ec4e6368d707df1b2b2d421dca83902
PethersonML <petherson.monteiro@hotmail.com>
2024-08-31 02:09:37 -0300

    Add route `GET /api/v1/postgresInfo`

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
 import dotenv from "dotenv";
 import express from "express";
+import databaseDatasRoutes from "./routes/databaseDatasRoutes";
 import database from "./infra/database.js";
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/api/v1/status', async (req, res) => {
-  const updatedAt = new Date().toISOString();
+app.use("api/v1", databaseDatasRoutes);
 
-  // const databaseInfos = await database.query("");
-  console.log("------------------------------ pg_stat_activity ------------------------------");
-  console.log(await database.query("SELECT * FROM pg_stat_activity"));
-  console.log("------------------------------------------------------------------------------")
+// app.get("/api/v1/status", async (req, res) => {
+//   const updatedAt = new Date().toISOString();
 
-  console.log("------------------------------ pg_stat_database ------------------------------");
-  console.log(await database.query("SELECT * FROM pg_stat_database"));
-  console.log("------------------------------------------------------------------------------")
+// console.log("------------------------------ pg_stat_activity ------------------------------");
+// console.log(await database.query("SELECT * FROM pg_stat_activity"));
+// console.log("------------------------------------------------------------------------------")
 
-  console.log("------------------------- pg_stat_database_conflicts -------------------------");
-  console.log(await database.query("SELECT * FROM pg_stat_database_conflicts"));
-  console.log("------------------------------------------------------------------------------")
+// console.log("------------------------------ pg_stat_database ------------------------------");
+// console.log(await database.query("SELECT * FROM pg_stat_database"));
+// console.log("------------------------------------------------------------------------------")
 
-  console.log("-------------------------- pg_stat_xact_user_tables --------------------------");
-  console.log(await database.query("SELECT * FROM pg_stat_xact_user_tables"));
-  console.log("------------------------------------------------------------------------------")
+// console.log("------------------------- pg_stat_database_conflicts -------------------------");
+// console.log(await database.query("SELECT * FROM pg_stat_database_conflicts"));
+// console.log("------------------------------------------------------------------------------")
 
-  console.log("------------------------------------ SHOW ------------------------------------");
-  console.log(await database.query("SHOW ALL"));
-  console.log("------------------------------------------------------------------------------")
+// console.log("-------------------------- pg_stat_xact_user_tables --------------------------");
+// console.log(await database.query("SELECT * FROM pg_stat_xact_user_tables"));
+// console.log("------------------------------------------------------------------------------")
 
-  res.status(200).json({
-    updated_at: updatedAt,
-  })
-})
+// console.log("------------------------------------ SHOW ------------------------------------");
+// const databaseInfos = await database.query("SHOW ALL");
+// console.log("------------------------------------------------------------------------------")
+
+//   res.status(200).json({
+//     updated_at: updatedAt,
+//     database_configs_limit: {
+//       configs: databaseInfos,
+//     },
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`Application running on http://localhost:${port}`);
-})
-
+});
